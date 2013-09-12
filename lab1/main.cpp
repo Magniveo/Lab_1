199c744a4171c0b2e5f4768e7c7f00cb723ffbcf
Mikhail Gerassimov <michailanatger@mail.ru>
2013-09-12 11:02:13 +0600

    Add lab1
    Fixed "counter++;" on "++counter":
    Add "denominator++;"Because was division on zero:

---
+++ lab1/main.cpp
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+int counter, denominator;
+
+denominator = counter = 0; //Устанавливаем начальные значение
+
+printf("Yvelichivaem shetchik i otobrajaem ego\n");
+printf("%d\n",++counter);//сначало увеличение счетчикаа потом вывод
+printf("Znachenie Counter/Denominator = ");
+denominator++;
+printf("%d\n",counter/denominator);
+printf("%d\n",counter);
+
+}
